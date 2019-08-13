0f9036796ab440036de43091689d81d4a7baa3e9
Cheng Chang <tonychangcheng@gmail.com>
2019-08-13 15:55:16 +1000

    use async/await

--- routes/weathers.js
+++ routes/weathers.js
@@ -1,64 +1,69 @@
-var express = require('express');
-var router = express.Router();
-var weatherRepository = require('../repositories/weathers');
+const express = require('express');
+const router = express.Router();
+const weatherRepository = require('../repositories/weathers');
 
-router.get('/', function (req, res, next) {
-  weatherRepository.getAll(req.query).then(docs => res.json(docs));
+router.get('/', async function (req, res, next) {
+  try {
+    let result = await weatherRepository.getAll(req.query);
+    res.json(result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-router.post('/', function (req, res, next) {
-  console.log(req.body);
-  // unique name check
-  weatherRepository.getAll({ name: req.body.name }).then(existingWeathers => {
+router.post('/', async function (req, res, next) {
+  try {
+    let existingWeathers = await weatherRepository.getAll({ name: req.body.name });
     if (existingWeathers.length > 0) {
       res.status(400).json('weather existed');
     } else {
-      weatherRepository.create(req.body).then(result => 
-        res.json(result)
-      ).catch(createError => 
-        res.status(500).send(createError)  
-      );
+      let result = await weatherRepository.create(req.body);
+      res.json(result);
     }
-  }).catch(getAllError => 
-    res.status(500).send(getAllError)  
-  )
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 });
 
-router.get('/:id', function (req, res, next) {
-  weatherRepository.getById(req.params.id).then(result => {
+router.get('/:id', async function (req, res, next) {
+  try {
+    let result = await weatherRepository.getById(req.params.id);
     console.log('result ===>>> ', result);
     if (!result) {
       res.sendStatus(404);
     } else {
       res.json(result);
     }
-  }).catch(error => 
-    res.status(500).send(error)
-  );
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 });
 
-router.patch('/:id', function (req, res, next) {
-  weatherRepository.patch(req.params.id, req.body).then(result => 
-    res.json(result)
-  ).catch(error => 
-    res.status(500).send(error)
-  );
+router.patch('/:id', async function (req, res, next) {
+  try {
+    let result = await weatherRepository.patch(req.params.id, req.body);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
 });
 
-router.put('/:id', function (req, res, next) {
-  weatherRepository.put(req.params.id, req.body).then(result => 
-    res.json(result)
-  ).catch(error => 
-    res.status(500).send(error)
-  );
+router.put('/:id', async function (req, res, next) {
+  try {
+    let result = await weatherRepository.put(req.params.id, req.body);
+    res.json(result);
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 });
 
-router.delete('/:id', function (req, res, next) {
-  weatherRepository.deleteById(req.params.id).then(result => 
-    res.json(result)
-  ).catch(error => 
-    res.status(500).send(error)
-  );
+router.delete('/:id', async function (req, res, next) {
+  try {
+    let result = await weatherRepository.deleteById(req.params.id);
+    res.json(result);
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 });
 
 module.exports = router;
